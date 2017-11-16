4bd58c3caf362954589db453e13879b3d89fe13f
Danielle Gellis <danisyellis@gmail.com>
2017-11-16 14:49:11 -0800

    add migrations folder. change requests table to timeoff_requests

---
+++ migrations/sqls/20171116224339-timeoff-request-table-down.sql
@@ -0,0 +1,11 @@
+/* Replace with your SQL commands */
+DROP TABLE timeoff_requests;
+
+CREATE TABLE requests(
+  id serial primary key,
+  username varchar(100) NOT NULL,
+  type_of_timeoff REASON NOT NULL,
+  date_off DATE NOT NULL,
+  time_start TIME NOT NULL,
+  time_end TIME NOT NULL
+);
